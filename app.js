7a9578a0e415bef2c60999cfcecef028567d26b4
Udi <udigeri@gmail.com>
2023-11-05 16:47:01 +0100

    add 'morgan' module for logging requests

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
+const morgan = require("morgan");
+
+morgan.token("body", function (req, res) {
+  return JSON.stringify(req.body);
+});
 
 app.use(cors());
 app.use(express.json());
+app.use(
+  morgan(
+    ":remote-addr :method :url :status :response-time ms - :res[content-length]bytes :body"
+  )
+);
 
 module.exports = app;
